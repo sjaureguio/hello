a66a192c1e8a5ba0f6e6b6c09567564ea21cf9e1
= <=>
2022-10-20 18:59:24 -0500

    Initial commit

---
+++ greet/greet.go
@@ -0,0 +1,11 @@
+package greet
+
+var name = "EDteam!"
+
+func English() string {
+	return "Hi " + name
+}
+
+func Italian() string {
+	return "Ciao" + name
+}
---
+++ greet/spanish.go
@@ -0,0 +1,6 @@
+package greet
+
+// Spanish Funación esportada que retorna saludo en español
+func Spanish() string {
+	return "Hola " + name
+}
